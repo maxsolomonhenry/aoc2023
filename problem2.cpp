b790173873e180358a2ccac1ceb18b7ea60cdb11
Max Henry <mhenry@eers.ca>
2023-12-20 08:27:46 -0500

    streamline solution part 2

--- problem2.cpp
+++ problem2.cpp
@@ -23,28 +23,34 @@ std::vector<std::string> parseByDelimiter(const std::string& line, const char& d
     return output;
 }
 
-int parseGameNo(std::string& game) {
-    game.erase(0, game.find(' '));
-    return std::stoi(game.substr(0, game.find(':')));
-}
-
-bool isValidGame(const std::vector<std::string>& handfuls, std::map<std::string, int>& limits) {
+int computePower(const std::vector<std::string>& handfuls) {
 
     int numCubes;
 
-    for (std::string handful : handfuls)
+    std::map<std::string, int> minCubesMap = {
+        {"red", 0}, {"green", 0}, {"blue", 0}
+    };
+
+    for (const std::string& handful : handfuls)
     {
         std::vector<std::string> cubes = parseByDelimiter(handful, ',');
-        for (std::string cube: cubes)
+        for (const std::string& cube: cubes)
         {
             std::vector<std::string> cubeInfo = parseByDelimiter(cube, ' ');
             numCubes = std::stoi(cubeInfo[0]);
-            if (numCubes > limits[cubeInfo[1]])
-                return false;
+
+            std::string& color = cubeInfo[1];
+
+            if (numCubes > minCubesMap[color])
+                minCubesMap[color] = numCubes;
         }
     }
 
-    return true;
+    int power = 1;
+    for (auto& pair: minCubesMap)
+        power *= pair.second;
+
+    return power;
 }
 
 int main() {
@@ -52,24 +58,13 @@ int main() {
     std::vector<std::string> lines;
     readFile("input/2.txt", lines);
 
-    int gameNo;
     int sum = 0;
-
-    std::map<std::string, int> limits = {
-        {"red", 12}, {"green", 13}, {"blue", 14}
-    };
-
+    
     for (std::string line : lines)
     {
         std::vector<std::string> info = parseByDelimiter(line, ':');
-
-        gameNo = parseGameNo(info[0]); 
-
         std::vector<std::string> handfuls = parseByDelimiter(info[1], ';');
-
-        if (isValidGame(handfuls, limits))
-            sum += gameNo;
-
+        sum += computePower(handfuls);
     }
 
     std::printf("Total sum: %d\n", sum);
