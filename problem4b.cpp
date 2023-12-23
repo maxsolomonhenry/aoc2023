ce032a13cc2836bb8d69ce4b8e594dc845c4cbf0
Max Henry <mhenry@eers.ca>
2023-12-23 13:15:14 -0500

    finish 4b

--- problem4b.cpp
+++ problem4b.cpp
@@ -2,80 +2,69 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <tuple>
 #include "util.h"
 
 
-class CardCounter {
-public:
-    CardCounter () {};
+int computeNumMatches(std::string line) {
+    std::vector<std::string> buffer;
+    std::vector<std::string> winningNumbers;
+    std::vector<std::string> myNumbers;
 
-    void parse(std::string line) {
-        std::vector<std::string> buffer;
-        std::vector<std::string> cardInfo;
-        std::vector<std::string> winningNosAsString;
-        std::vector<std::string> myNosAsString;
-        std::vector<std::string> allNosAsString;
-        int cardNo;
+    buffer = parseByDelimiter(line, ':');
+    buffer = parseByDelimiter(buffer[1], '|');
 
-        buffer = parseByDelimiter(line, ':');
-        cardInfo = parseByDelimiter(buffer[0], ' ');
-        allNosAsString = parseByDelimiter(buffer[1], '|');
-        winningNosAsString = parseByDelimiter(allNosAsString[0], ' ');
-        myNosAsString = parseByDelimiter(allNosAsString[1], ' ');
+    winningNumbers = parseByDelimiter(buffer[0], ' ');
+    myNumbers = parseByDelimiter(buffer[1], ' ');
 
-        cardNo = std::stoi(cardInfo[1]);
-        std::cout << cardNo << "\n";
+    std::vector<int> winningIntegers;
+    std::vector<int> myIntegers;
 
-        std::vector<int> winningNos;
-        std::vector<int> myNos;
+    std::transform(winningNumbers.begin(), winningNumbers.end(), 
+        std::back_inserter(winningIntegers), [](std::string s) { return std::stoi(s); });
 
-        std::transform(winningNosAsString.begin(), winningNosAsString.end(), 
-            std::back_inserter(winningNos), [](std::string s) { return std::stoi(s); });
+    std::transform(myNumbers.begin(), myNumbers.end(), 
+        std::back_inserter(myIntegers), [](std::string s) { return std::stoi(s); });
 
-        std::transform(myNosAsString.begin(), myNosAsString.end(), 
-            std::back_inserter(myNos), [](std::string s) { return std::stoi(s); });
+    std::sort(winningIntegers.begin(), winningIntegers.end());
+    std::sort(myIntegers.begin(), myIntegers.end());
 
-        std::sort(winningNos.begin(), winningNos.end());
-        std::sort(myNos.begin(), myNos.end());
+    std::vector<int> intersection;
 
-        std::vector<int> intersection;
+    std::set_intersection(
+        winningIntegers.begin(), winningIntegers.end(),
+        myIntegers.begin(), myIntegers.end(),
+        std::back_inserter(intersection));
 
-        std::set_intersection(
-            winningNos.begin(), winningNos.end(),
-            myNos.begin(), myNos.end(),
-            std::back_inserter(intersection));
+    int nMatches = intersection.size();
 
-        int nMatches = intersection.size();
-
-        for (int i = 1; i <= nMatches; ++i)
-        {
-            int idx = (cardNo + 1) + i;
-            nCopies_[idx] += nCopies_[cardNo]; 
-        }
-    }
-
-    int total() {
-        int sum = 0;
-        for (int n : nCopies_)
-            sum += n;
-        return sum;
-    }
-
-private:
-    std::vector<int> nCopies_;
-};
+    return nMatches;
+}
 
 int main() {
 
     std::vector<std::string> lines;
     readFile("input/4.txt", lines);
 
-    CardCounter counter;
+    int nLines = lines.size();
+    std::vector<int> nCopies(nLines, 1);
 
-    for (std::string line : lines) 
-        counter.parse(line);
+    for (int i = 0; i < nLines; ++i) 
+    {
+        int nMatches = computeNumMatches(lines[i]);
 
-    // std::cout << "Total sum: " << counter.total() << "\n";
+        for (int j = 1; j <= nMatches; ++j)
+        {
+            int idx = i + j;
+            nCopies[idx] += nCopies[i];
+        }
+    }
+
+    int sum = 0;
+    for (int n : nCopies)
+        sum += n;
+
+    std::cout << "Total sum: " << sum << "\n";
 
     return 0;
 } 
